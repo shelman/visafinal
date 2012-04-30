e6672724a6ca09eb7fd2000dea83e108acb30a89
Sam Helman <sam.d.helman@gmail.com>
2012-04-30 00:36:46 -0400

    game logic done

--- gerald.js
+++ gerald.js
@@ -6,7 +6,6 @@ var Gerald = {
 		
 		var GERALD_WIDTH = 80,
 			GERALD_HEIGHT = 100;
-
 		// make the world
 		Crafty.init(700, 700);
 		Crafty.background('rgb(127,127,127)');
@@ -16,21 +15,28 @@ var Gerald = {
 		this.ab1 = $('#ab1').hide();
 		this.ab2 = $('#ab2').hide();
 		this.ab3 = $('#ab3').hide();
+		this.item1 = $('#item1').hide();
+		this.item2 = $('#item2').hide();
+		this.item3 = $('#item3').hide();
+		this.item4 = $('#item4').hide();
+		this.items = [];
+		this.numItems = 0;
 
 		// make gerald
 		var gerald = this.gerald = Crafty.e("Gerald, 2D, DOM, Color, Collision, Multiway")
 			.color('rgb(0,0,0)')
-			.attr({ x: 50, y: 480, w: GERALD_WIDTH, h: GERALD_HEIGHT, z: 10 })
 			.multiway(8, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
 		
-		gerald.bind('Moved', function(from) {
+		gerald.movedFunc = function(from) {
 			self.clearCollision();
 			var itemHitAsArr;
 			if ((itemHitAsArr = this.hit('GCollidable'))) {
 				this.attr({ x:from.x, y:from.y });	
 				self.triggerCollision(itemHitAsArr[0].obj);
 			}
-		});
+		};
+		
+		gerald.bind('Moved', gerald.movedFunc);
 		
 		// make the four walls 
 		this.topWall = Crafty.e("2D, DOM, Collision, GCollidable")
@@ -68,6 +74,10 @@ var Gerald = {
 				case 'diningRoom':
 					self.createDiningRoom(collidedObj.positionOpp);
 					break;	
+				case 'end':
+					self.done = true;
+					self.createBedroom('start');
+					break;
 			}
 			
 			self.clearText();
@@ -111,6 +121,16 @@ var Gerald = {
 		this.createDoors(['right', 'top'], ['bathroom', 'hallway']);
 			
 		this.setText("Where am I?  This isn't my house... something feels wrong.  I should probably get out of here.");
+		
+		if (this.done) {
+			var self = this;
+			setTimeout(function() {
+				self.gerald.disableControl();
+				self.gerald.unbind('Moved', self.gerald.movedFunc);
+				self.setText("Where am I?  This isn't my house... something feels wrong.  I should probably get out of here....");
+				$('#cr-stage').fadeOut(7000);
+			}, 50);	
+		}
 	},
 	
 	createBathroom: function(startPoint) {
@@ -202,7 +222,7 @@ var Gerald = {
 		this.currObjects.push(table);
 		
 		// sunflower painting
-		var drSfPainting = this.drSfPainting = Crafty.e("drSfPainting, 2D, DOM, Color, Collision, GCollidable")
+		var drSfPainting = this.drSfPainting = Crafty.e("Tween, drSfPainting, 2D, DOM, Color, Collision, GCollidable")
 			.color('rgb(100,100,100)')
 			.attr({ x: 530, y: 300, w: 160, h: 100, z: 5 });
 		this.currObjects.push(drSfPainting);
@@ -261,6 +281,7 @@ var Gerald = {
 					
 		switch (startPoint) {
 			case 'start':
+				this.gerald.attr({ x: 50, y: 480, w: 80, h: 100, z: 10 });
 				break;
 			case 'right':
 				this.gerald.attr({ x: 600, y: 300, w: 80, h: 100, z: 10 });
@@ -315,13 +336,19 @@ var Gerald = {
 			case this.ksink:
 				this.triggerKSink();
 				break;
+			case this.hallSfPainting:
+				this.triggerPainting();
+				break;
+			case this.table:
+				this.triggerTable();
+				break;
 		}	
 	},
 	
 	clearCollision: function() {
-		this.ab1.hide();
-		this.ab2.hide();
-		this.ab3.hide();
+		this.ab1.hide().off('click');
+		this.ab2.hide().off('click');
+		this.ab3.hide().off('click');
 		this.clearText();	
 	},
 	
@@ -338,9 +365,24 @@ var Gerald = {
 		var self = this;
 		this.ab1.html('Look under bed');
 		this.ab1.show();
-		this.ab1.on('click', function() {
-			self.setText("It's too dark under the bed to see anything.");
-		});
+		if (this.hasItem('flashlightOn')) {
+			this.ab1.on('click', function() {
+				if (self.hasItem('key')) {
+					if (self.hasItem('woodCarving')) {
+						self.setText("Nothing left in here");
+					} else {
+						self.setText("The key fit the chest under the bed perfectly... all that's inside is some sort of wooden carving.");
+						self.getItem('woodCarving');
+					}
+				} else {
+					self.setText("Hey, there's a locked chest deep under the bed.  Never would have seen that without the flashlight.");	
+				}
+			});	
+		} else {
+			this.ab1.on('click', function() {
+				self.setText("It's too dark under the bed to see anything.");
+			});
+		}
 		
 		this.ab2.html('Look under sheets');
 		this.ab2.show();
@@ -401,15 +443,38 @@ var Gerald = {
 		this.ab1.on('click', function() {
 			self.setText("Whoa!  That water's way too hot.");
 		});
+		
+		if (this.items.indexOf('dirtyDish') != -1) {
+			this.ab2.html('Wash dirty dish');
+			this.ab2.show();
+			this.ab2.on('click', function() {
+				self.setText("That did the trick!  Now that the plate's clean, it looks like it has some sort of series of numbers inscribed on it...");
+				if (self.items.indexOf('cleanDish') == -1) {
+					self.items[self.items.indexOf('dirtyDish')] = 'cleanDish';
+				}
+			});
+		}
 	},
 	
 	triggerFridge: function() {
 		var self = this;
 		this.ab1.html('Look in fridge');
 		this.ab1.show();
-		this.ab1.on('click', function() {
-			self.setText("Just some cheese and batteries.  This person really needs to go shopping.");
-		});	
+		if (this.items.indexOf('flashlight') != -1) {
+			this.ab1.on('click', function() {
+				self.setText("Good thing there were batteries in the fridge.  They fit the flashlight perfectly.");
+				self.items[self.items.indexOf('flashlight')] = 'flashlightOn';
+			});	
+		} else if (this.items.indexOf('flashlightOn') != -1) {
+			this.ab1.on('click', function() {
+				self.setText("Nothing left but the cheese.");
+			});	
+		} else {
+			this.ab1.on('click', function() {
+				self.setText("Nothing in here but cheese and batteries.  This person really needs to go shopping.");
+			});	
+		}
+
 	},
 	
 	triggerStove: function() {
@@ -422,9 +487,19 @@ var Gerald = {
 		
 		this.ab2.html('Look in oven');
 		this.ab2.show();
-		this.ab2.on('click', function() {
-			self.setText("Who left a flashlight in here?  Won't turn on though.");
-		});	
+		if (this.items.indexOf('flashlight') == -1 && this.items.indexOf('flashlightOn') == -1) {
+			this.ab2.on('click', function() {
+				self.setText("Who left a flashlight in here?  Won't turn on though.");
+				if (self.items.indexOf('flashlight') == -1) {
+					self.getItem('flashlight');
+				}
+			});	
+		} else {
+			this.ab2.on('click', function() {
+				self.setText('Nothing interesting in here.');
+			});
+		}
+
 	},
 	
 	triggerKSink: function() {
@@ -438,9 +513,67 @@ var Gerald = {
 		this.ab2.html('Inspect dishes');
 		this.ab2.show();
 		this.ab2.on('click', function() {
-			self.setText("Hmm... one of these plates is filthy, but it looks like something's written on it under the caked food.");
+			if (self.items.indexOf('dirtyDish') == -1 && self.items.indexOf('cleanDish') == -1) {
+				self.getItem('dirtyDish');
+			}
+			self.setText("These dishes are filthy.");
 		});
-	}
+	},
+	
+	triggerTable: function() {
+		var self = this;
+		this.ab1.html('Sit down');
+		this.ab1.show();
+		this.ab1.on('click', function() {
+			self.setText("No time to rest.  But wait... there's a weird spiral indentation in the table.");
+		});
+		
+		if (this.hasItem('woodCarving') && !this.canExit) {
+			this.ab2.html('Insert Carving');
+			this.ab2.show();
+			this.ab2.on('click', function() {
+				if (!this.canExit) {
+					self.setText("The carving fits the weird indentation in the table perfectly.  Umm... what's happening to that painting?");
+					self.drSfPainting.tween({ alpha: 0.0,  }, 100);
+					setTimeout(function() {
+						self.drSfPainting.destroy();
+					}, 1000);
+					self.createDoors(['right'], ['end']);
+				}
+			});
+
+		}
+	},
+	
+	triggerPainting: function() {
+		var self = this;
+		this.ab1.html('Examine painting');
+		this.ab1.show();
+		this.ab1.on('click', function() {
+			self.setText("That is one weird painting.");
+		});
+		
+		this.ab2.html('Poke painting');
+		this.ab2.show();
+		this.ab2.on('click', function() {
+			self.setText("Holy shit, there's a safe behind here.");
+			if (self.hasItem('cleanDish') && !self.hasItem('key')) {
+				self.setText("The combination from the plate worked on the safe behind the painting... there's a key inside.  Cool!");
+				self.getItem('key');
+			}
+		});	
+	},
+	
+	getItem: function(item) {
+		this.items[this.numItems] = item;
+		this.numItems += 1;	
+		this['item' + this.numItems].show();
+	},
+	
+	hasItem: function(item) {
+		return this.items.indexOf(item) != -1;	
+	} 
+	
 };
 
 $(document).ready(function() {
